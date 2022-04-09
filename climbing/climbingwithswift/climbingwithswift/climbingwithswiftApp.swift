b3d93ab1228bc04220149c9daea3393f1fd589ef
kch2583 <kch2583@naver.com>
2022-04-09 22:00:34 +0900

    초기설정

---
+++ climbing/climbingwithswift/climbingwithswift/climbingwithswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  climbingwithswiftApp.swift
+//  climbingwithswift
+//
+//  Created by ch on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct climbingwithswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sidmool-vuetify/src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App.vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import axios from "axios";
+Vue.use(Vuetify);
+
+Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
+
+new Vue({
+  el: "#app",
+  vuetify: new Vuetify(),
+  axios,
+  render: h => h(App)
+});
